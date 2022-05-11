0fa6601ac9d0cb9b806811074f0be69cc7fa1bea
Yudi Yamane <yudiyamane@gmail.com>
2022-05-11 06:19:18 -0300

    ship moves correctly

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Asteroids
 
 Godot 3.4.4
+
+[Inspiração](https://www.youtube.com/watch?v=_TKiRvGfw3Q)
--- game/player/player.gd
+++ game/player/player.gd
@@ -1,9 +1,9 @@
 extends Area2D
 
-var speed := 0
-var velocity := Vector2.RIGHT
+var speed := 100
+var velocity := Vector2.ZERO
 var direction := Vector2.UP
-var angular_speed := deg2rad(50)
+var angular_speed := deg2rad(100)
 var speed_x := 2
 var speed_y := 2
 
@@ -13,26 +13,25 @@ func _ready():
 
 func _process(delta):
 	update()
-	var dx = 0
-	var dy = 0
 	if Input.is_action_pressed("rotate_left"):
 		direction = direction.rotated(-angular_speed*delta)
 	if Input.is_action_pressed("rotate_right"):
 		direction = direction.rotated(angular_speed*delta)
 
-#	$AnimatedSprite.rotation = direction.angle()
+	$AnimatedSprite.rotation = direction.rotated(PI/2).angle()
 	
-	if velocity.length() < 500:
-		velocity += velocity*velocity.dot(direction)
 	if Input.is_action_pressed("accelerate"):
-		position += velocity*speed*delta
+		velocity += direction*speed*delta
 	
-	velocity = velocity.normalized()*clamp(velocity.length(), 0, 50)
+	position.x += velocity.x*delta
+	position.y += velocity.y*delta
 	
-	$Direction.text = str(rad2deg(direction.angle()))
+	$Direction.text = str(int(rad2deg(direction.angle())))
+	$Direction.text += '\n' + str(int(rad2deg($AnimatedSprite.rotation)))
+	$Speed.text = str(int(velocity.length()))
 	
 func _draw():
 	var from = Vector2.ZERO
 	var corrected_dir = direction.rotated(0)*25
-	draw_line(from, velocity*100, Color.red, 3)
+	draw_line(from, velocity, Color.red, 3)
 	draw_line(from, corrected_dir, Color.yellow, 3)
