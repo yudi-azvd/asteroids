7955412d04ebe1f245d7ac5ff5c4fd89ffb574e4
Yudi Yamane <yudiyamane@gmail.com>
2022-05-12 04:38:42 -0300

    shooting works with small bug

--- game/player/bullet.gd
+++ game/player/bullet.gd
@@ -1,34 +1,38 @@
 extends Area2D
 
-signal shoot_bullet
-
 var touch_limit_count = 0
 var speed = 200
 var velocity = Vector2.UP*4
 var can_move = false
+onready var window_height = get_viewport().size.y
+onready var window_width = get_viewport().size.x
 
+
+	
 func _process(dt):
-	position += velocity*dt*speed
-#	print(position)
+	if can_move:
+		position += velocity*dt*speed
 
 func _on_Bullet_area_entered(area: Area2D):
 	if not area.name.begins_with("Limit"):
 		return
 
-	print("touched limit ", area.name)
+	print(position)
+	print("touch limit ", area.name)
 	touch_limit_count += 1
-	
-	print("pos y ", position.y)
 	if area.name.ends_with("Top"):
-#		position.y = get_viewport().size.y/2 +100
-		pass
+		position.y = window_height/2
 	if area.name.ends_with("Bottom"):
-		position.y = -get_viewport().size.y/2
-
+		position.y = -window_height/2
+	if area.name.ends_with("Right"):
+		position.x = -window_width/2
+	if area.name.ends_with("Left"):
+		position.x = window_width/2
+	print(position)
+		
 	if touch_limit_count == 2:
 		print("free bullet")
 		queue_free()
 
-
 func _on_Bullet_shoot_bullet():
-	pass # Replace with function body.
+  can_move = true
--- game/player/player.gd
+++ game/player/player.gd
@@ -4,11 +4,11 @@ var speed := 100
 var velocity := Vector2.ZERO
 var direction := Vector2.UP
 var angular_speed := deg2rad(200)
-export(PackedScene) var bullet_scene
 var bullet_timer = 0
+var Bullet = preload("res://game/player/bullet.tscn")
 
 func _ready():
-	pass
+	$Bullet.hide()
 
 func _process(delta):
 	bullet_timer += delta
@@ -24,14 +24,27 @@ func _process(delta):
 		$AnimatedSprite.animation = "moving"
 	else:
 		$AnimatedSprite.animation = "idle"
-		
+
+	if Input.is_action_pressed("shoot") and bullet_timer > 0.5:
+		shoot()
+			
 	position += velocity*delta
 	
 	$Direction.text = str(int(rad2deg(direction.angle())))
 	$Speed.text = str(int(velocity.length()))
 
 	update()
-	
+
+func shoot():
+	bullet_timer = 0
+	var new_bullet = Bullet.instance()
+	new_bullet.position = Vector2.ZERO
+	new_bullet.rotation = direction.rotated(PI/2).angle()
+	new_bullet.velocity = direction*4
+	new_bullet.can_move = true
+	new_bullet.show()
+	add_child(new_bullet)
+		
 func _draw():
 	var from = Vector2.ZERO
 	var corrected_dir = direction.rotated(0)*25
