3ee651b8601a18c02008da4a4274bfa361a85a0a
Yudi Yamane <yudiyamane@gmail.com>
2022-05-12 07:32:33 -0300

    shooting bullet works correctly with global_position

--- docs/README.md
+++ docs/README.md
@@ -1,3 +1,14 @@
+## Local vs Global
+
+`position` é relativo ao nó em si. Apropriado para fazer deslocamento.
+
+`global_position` é relativo ao espaço global. Apropriado para fazer cálculos
+com dimensões da tela(?).
+
+https://godotengine.org/qa/40058/what-is-the-difference-between-global-and-local-coordinates
+
+## Ícones
+
 Gerar ícones para diferentes plataformas.
 
 https://docs.godotengine.org/en/stable/tutorials/export/changing_application_icon_for_windows.html
--- game/player/bullet.gd
+++ game/player/bullet.gd
@@ -1,12 +1,12 @@
 extends Area2D
 
 var touch_limit_count = 0
-var speed = 200
+var speed = 50
 var velocity = Vector2.UP*4
 var can_move = false
 onready var window_height = get_viewport().size.y
 onready var window_width = get_viewport().size.x
-	
+
 func _process(dt):
 	if can_move:
 		position += velocity*dt*speed
@@ -17,17 +17,13 @@ func _on_Bullet_area_entered(area: Area2D):
 
 	touch_limit_count += 1
 	if area.name.ends_with("Top"):
-#		position.y = window_height/2
-		position.y = window_height
+		global_position.y = window_height
 	if area.name.ends_with("Bottom"):
-#		position.y = -window_height/2
-		position.y = 0
+		global_position.y = 0
 	if area.name.ends_with("Right"):
-#		position.x = -window_width/2
-		position.x = 0
+		global_position.x = 0
 	if area.name.ends_with("Left"):
-#		position.x = window_width/2
-		position.x = window_width
+		global_position.x = window_width
 	
 	if touch_limit_count == 2:
 		queue_free()
