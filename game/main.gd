39ffbb3555d0f8cd40c3f1235638e5954c7e9d5e
Yudi Yamane <yudiyamane@gmail.com>
2022-08-25 22:55:31 -0300

    chore: sinal do player conectado à main

--- game/main.gd
+++ game/main.gd
@@ -3,9 +3,10 @@ extends Node
 var AsteroidSpawner = preload("res://game/asteroids/AsteroidSpawner.gd")
 
 func _ready():
+	$Player.connect("player_died", self, "on_Player_died")
 #	$AsteroidSpawner._asteroid_spawn_interval_in_seconds = INF
 	$AsteroidSpawner/SFXExplosion.stop()
-	pass
+	
 
 func on_Player_died():
 	print('Player died')
